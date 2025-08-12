ae6f3ed3445fbb231121b9830bdfec3c05b446b9
Techno263 <techno263code@gmail.com>
2025-08-12 02:27:20 -0400

    add macro for string literal error msg

--- src/mica/try.hpp
+++ src/mica/try.hpp
@@ -35,3 +35,35 @@ do { \
 
 #define MICA_TRY_VOID(expr_) \
     _MICA_INTERNAL_TRY_VOID(expr_, MICA_TMP_VAR_DEFAULT)
+
+#define _MICA_INTERNAL_TRY_STATIC(result_, expr_, err_msg_, tmp_exp_var_) \
+do { \
+    auto&& tmp_exp_var_ = (expr_); \
+    static_assert(mica::is_expected_v<std::remove_reference_t<decltype(tmp_exp_var_)>>); \
+    static_assert(std::is_same_v< \
+        std::remove_reference_t<decltype(result_)>, \
+        std::remove_reference_t<decltype(tmp_exp_var_)>::value_type> \
+    ); \
+    static_assert(mica::is_string_literal(err_msg_)); \
+    if (!tmp_exp_var_.has_value()) [[unlikely]] { \
+        return std::unexpected(err_msg_); \
+    } \
+    result_ = std::move(tmp_exp_var_).value(); \
+} while (0)
+
+#define MICA_TRY_STATIC(result_, expr_, err_msg_) \
+    _MICA_INTERNAL_TRY_STATIC(result_, expr_, err_msg_, MICA_TMP_VAR_DEFAULT)
+
+#define _MICA_INTERNAL_TRY_STATIC_VOID(result_, expr_, err_msg_, tmp_exp_var_) \
+do { \
+    auto&& tmp_exp_var_ = (expr_); \
+    static_assert(mica::is_expected_v<std::remove_reference_t<decltype(tmp_exp_var_)>>); \
+    static_assert(std::is_void_v<std::remove_reference_t<decltype(tmp_exp_var_)>::value_type>); \
+    static_assert(mica::is_string_literal(err_msg_)); \
+    if (!tmp_exp_var_.has_value()) [[unlikely]] { \
+        return std::unexpected(err_msg_); \
+    } \
+} while (0)
+
+#define MICA_TRY_STATIC_VOID(result_, expr_, err_msg_) \
+    _MICA_INTERNAL_TRY_STATIC_VOID(result_, expr_, err_msg_, MICA_TMP_VAR_DEFAULT)
--- src/mica/type_traits.hpp
+++ src/mica/type_traits.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cstddef>
 #include <expected>
+#include <string>
 #include <type_traits>
 
 namespace mica {
@@ -15,5 +17,23 @@ struct is_expected<std::expected<T, E>> : std::true_type
 
 template<typename T>
 constexpr bool is_expected_v = is_expected<T>::value;
+
+template<typename T, typename = void>
+struct is_char_type : std::false_type
+{};
+
+template<typename T>
+struct is_char_type<T, std::void_t<typename std::char_traits<T>::char_type>> : std::true_type
+{};
+
+template<typename T>
+constexpr bool is_char_type_v = is_char_type<T>::value;
+
+template<typename CharT, std::size_t N>
+struct is_string_literal<const CharT (&)[N]> : std::bool_constant<is_char_type<CharT>::value>
+{};
+
+template<typename CharT, std::size_t N>
+constexpr bool is_string_literal_v = is_string_literal<CharT, N>::value;
 
 } // namespace mica
